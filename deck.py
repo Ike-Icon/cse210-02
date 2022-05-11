c671ed355d5227bd1aa1f860754d1200bd482607
Zeus-Max-Code <hazardos10@gmail.com>
2022-05-11 20:59:21 +0000

    After adding a few things

--- deck.py
+++ deck.py
@@ -1,23 +1,38 @@
 import random
+import random
 
-# TODO: Implement the Die class as follows...
-
-# 1) 
 class Deck:
-    """
-   
-   
-    """
-
-    # 2) 
     def __init__(self):
-        self.value = 0
-    """
+        self.card = 0 #card number
+        self.score = 300 #total player score
+    
+    def draw_card(self):
+        self.card = random.randint(1,13) #create a card with a random number between 1-13
+        return int(self.card)
+      
 
+    def award_points(self):
+        self.score += 100 #points awarded if guess is correct
+    
+    def deduct_points(self):
+        self.score = self.score - 75 #points deducted if guess is incorrect
+        
+    
+    #return integer of score so that it can be compare in start_game.py
+    def game_over_score(self):
+        game_over = self.score
+        return int(game_over)
 
-    """
-    # 3) 
-    def pick(self):
-        """
-    """
-        self.value = random.randint(1,13)
+    
+    # Function to print the scorebaord  
+    def print_scoreboard(self):
+   
+        print("\t\t\t     ____________________")
+        print("\t\t\t    |                    |")
+        if self.score <= 0:
+            print("\t\t\t    |  Your Score = {}  |".format(self.score))
+        else:   
+            print("\t\t\t    |  Your Score = {}  |".format(self.score))
+          
+        print("\t\t\t    |____________________|")
+
--- main.py
+++ main.py
@@ -1,4 +1,60 @@
 from Director import Director
+from Director import Deck
 
 director = Director()
-director.start_game()
+deck = Deck()
+keep_playing = 'y'
+
+while keep_playing == 'y':
+    card_deck = director.cards() #brings card_deck list using cards function
+
+    #print(card_deck) left this here for debbugging purposes
+
+    guess = input('Higher or lower? [h/l] ').lower()
+
+    print("\t\t\t     ____________________")
+    print("\t\t\t                         ")  
+    print("\t\t\t     The Next Card was {}".format(card_deck[1]))   
+    print("\t\t\t     ____________________")
+    print("\t\t\t                         ") 
+
+    #compares the 2 card values in the card deck list
+    # first card will be position [0] and second card will be position [1]
+
+    if guess == 'h' and (card_deck[1] > card_deck[0]):
+        deck.award_points()
+        print("\t\t\t    Your score is plus 100 now!")
+    elif guess == 'h' and (card_deck[1] < card_deck[0]):
+        deck.deduct_points()
+        print("\t\t\t    Your score is minus 75 now!")
+    elif guess == 'l' and (card_deck[1] < card_deck[0]):
+        deck.award_points()
+        print("\t\t\t    Your score is plus 100 now!")
+    elif guess == 'l' and (card_deck[1] > card_deck[0]):
+        deck.deduct_points()
+        print("\t\t\t    Your score is minus 75 now!")
+    elif guess == 'h' and (card_deck[1] == card_deck[0]):
+        deck.deduct_points()
+        print("\t\t\t    Your score is minus 75 now!")
+    elif guess == 'l' and (card_deck[1] == card_deck[0]):
+        deck.deduct_points()
+        print("\t\t\t    Your score is minus 75 now!")
+
+
+    card_deck.clear()
+    #clears deck for new loop
+
+    # print the scoreboad for the user
+    deck.print_scoreboard()
+
+        #finds score so that it can be compared
+    score = deck.game_over_score()
+
+        #if player reaches score of 0 or less then the game is over
+    if score <= 0:
+        keep_playing = 'n'
+        #if score is greater than 0 they can continue to play
+    else:
+        keep_playing = input('Play again? [y/n]: ').lower()
+
+    print()
